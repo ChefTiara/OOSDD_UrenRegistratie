94fdec33df04cda847faf49f57b2ffc87685f7d0
velocity968 <s1188324@student.windesheim.nl>
2025-11-25 10:29:01 +0100

    Added Declarationpage
    Added Declaration page, Model and Service

--- Hourregistration.App/Views/Page1.xaml.cs
+++ Hourregistration.App/Views/Page1.xaml.cs
@@ -1,9 +1,13 @@
+using Hourregistration.Core.Models;
 using Hourregistration.App.Services;
+using Hourregistration.Core.Services;
 
 namespace Hourregistration.App.Views;
 
 public partial class Page1 : ContentPage
 {
+    private readonly DeclarationService _service;
+
     public Page1()
     {
         InitializeComponent();
@@ -26,45 +30,33 @@ public partial class Page1 : ContentPage
         await Navigation.PopAsync();
     }
 
-    private async void OnGoToPage2(object sender, EventArgs e)
-    {
-        if (SessionManager.CanAccessPage(2))
-        {
-            await Navigation.PushAsync(new Page2());
-        }
-        else
-        {
-            await DisplayAlert("Geen toegang", "Je hebt geen toegang tot Pagina 2.", "OK");
-        }
-    }
-
-    private async void OnGoToPage3(object sender, EventArgs e)
-    {
-        if (SessionManager.CanAccessPage(3))
-        {
-            await Navigation.PushAsync(new Page3());
-        }
-        else
-        {
-            await DisplayAlert("Geen toegang", "Je hebt geen toegang tot Pagina 3.", "OK");
-        }
-    }
-
-    private async void OnGoToPage4(object sender, EventArgs e)
-    {
-        if (SessionManager.CanAccessPage(4))
-        {
-            await Navigation.PushAsync(new Page4());
-        }
-        else
-        {
-            await DisplayAlert("Geen toegang", "Je hebt geen toegang tot Pagina 4.", "OK");
-        }
-    }
-
     private void OnLogout(object sender, EventArgs e)
     {
         SessionManager.CurrentRole = null;
         Application.Current.MainPage = new NavigationPage(new LoginPage());
+    }
+
+    private void OnIndienenClicked(object sender, EventArgs e)
+    {
+        // Validatie van invoer uren
+        if (!double.TryParse(UrenEntry.Text, out double uren))
+        {
+            FeedbackLabel.Text = "Voer een geldig aantal in.";
+            FeedbackLabel.TextColor = Colors.Red;
+            return;
+        }
+
+        // Maak declaratie object
+        var declaratie = new Declaration
+        {
+            Datum = DatumPicker.Date,
+            AantalUren = uren
+        };
+
+        // Valideer via service
+        var result = _service.Indienen(declaratie);
+
+        FeedbackLabel.Text = result.Message;
+        FeedbackLabel.TextColor = result.Success ? Colors.Green : Colors.Red;
     }
 }
---
+++ Hourregistration.Core/Models/Declaration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hourregistration.Core.Models
+{
+    public class Declaration
+    {
+        public double AantalUren { get; set; }
+        public DateTime Datum { get; set; }
+    }
+}

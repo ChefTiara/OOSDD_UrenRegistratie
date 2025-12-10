ce616fc16dbefce842905aba5ce7040fb764ac25
Jim-Wetering <145760054+Jim-Wetering@users.noreply.github.com>
2025-12-10 12:08:45 +0100

    New Layout UrenbeoordelingPage and Integration with its ViewModel

--- Hourregistration.App/Views/UrenbeoordelingPage.xaml.cs
+++ Hourregistration.App/Views/UrenbeoordelingPage.xaml.cs
@@ -1,9 +1,28 @@
+using Hourregistration.App.ViewModels;
+
 namespace Hourregistration.App.Views;
 
 public partial class UrenbeoordelingPage : ContentPage
 {
-	public UrenbeoordelingPage()
-	{
-		InitializeComponent();
-	}
+    public UrenbeoordelingPage(UrenbeoordelingViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (BindingContext is UrenbeoordelingViewModel vm)
+            vm.OnAppearing();
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+
+        if (BindingContext is UrenbeoordelingViewModel vm)
+            vm.OnDisappearing();
+    }
 }
